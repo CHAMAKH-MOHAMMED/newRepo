3d0627e5481ead9dca70b0c8c797c473871d9106
mohammed <yaansimo@gmail.com>
2025-02-19 23:54:16 +0100

    add calcul class to project

---
+++ Calcul.java
@@ -0,0 +1,20 @@
+public class Calcul {
+    
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+    public static int soustraction(int a, int b) {
+        return a - b;
+    }
+    public static int multiplication(int a, int b) {
+        return a * b;
+    }
+    public static int division(int a, int b) {
+        return a / b;
+    }
+
+    
+}
+
+
+
